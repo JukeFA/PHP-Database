85ccdd756bc6b5820ad83c3a6d8ac2e248190370
JukeFA <45007798+JukeFA@users.noreply.github.com>
2018-12-04 09:56:08 -0500

    Bug Fixes

--- Local Site/Array Basics.php
+++ Local Site/Array Basics.php
@@ -1,24 +1,25 @@
 <?php
-$cars = array("Volvo", "BMW", "Volkswagen");
+  $cars = array("Volvo", "BMW", "Volkswagen");
 
-echo "I like " .$cars[0]. ", " .$cars[1]. ", " .$cars[2]. ".";
+  echo "I like " .$cars[0]. ", " .$cars[1]. ", " .$cars[2]. ".";
 
 
-$arrlength = count($cars);
-for($x=0; $x < $arrlength; $x++) {
-  echo $cars[$x];
-  echo "<br>";
+  $arrlength = count($cars);
+  for($x=0; $x < $arrlength; $x++) {
+    echo $cars[$x];
+    echo "<br>";
 }
 
 
 
-$age = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43");
-echo "Peter is " .$age['Peter']. "years old.";
-
-
-foreach($age as $x => $x_value) {
-  echo "Key=" .$x. ", Value=" .$x_value;
+  $age = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43");
+  echo "Peter is " .$age['Peter']. "years old.";
   echo "<br>";
+
+
+  foreach($age as $x => $x_value) {
+    echo "Key=" .$x. ", Value=" .$x_value;
+    echo "<br>";
 }
 
 ?>
--- Local Site/Background Color.php
+++ Local Site/Background Color.php
@@ -4,7 +4,7 @@
 <meta charset="utf-8">
 <title>Background</title>
 </head>
-<?
+<?php
   $today = date("l");
   print("$today");
 
